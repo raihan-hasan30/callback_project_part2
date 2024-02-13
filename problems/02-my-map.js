dd1ab2fc547248525d9895277fe6a69929fd54c4
Noah <noah.j.ragan@gmail.com>
2024-02-12 19:07:35 -0600

    Completed Problems 1 & 2

--- problems/02-my-map.js
+++ problems/02-my-map.js
@@ -15,12 +15,21 @@ let result2 = myMap(['run', 'Forrest'], function (el) {
 });
 console.log(result2);   // [ 'RUN!', 'FORREST!' ]
 *******************************************************************************/
-
+/*This Line declares a function named 'myMap' that takes two parameters: 'array' (the array to be mapped) and 'cb'
+(the callback function to be called for each element of the array)*/
 function myMap(array, cb) {
+  /*This line initializes an empty array named 'result' where we'll store the results of applying the 
+    callback function to each element of the input array*/
   let result = [];
+  /*This line starts a for loop that iterates over the elements of the input array. 
+  It initializes a variable i to 0, and the loop continues as long as i is less than the length of the array. After each iteration, i is incremented by 1.*/
   for (let i = 0; i < array.length; i++) {
+    /*Inside the loop, this line calls the callback function cb with the current element of the input array (array[i]). 
+    It pushes the result of applying the callback function to the current element into the result array.*/
     result.push(cb(array[i]));
   }
+  /*This line marks the end of the for loop and the myMap function definition. 
+  It returns the result array, which contains the mapped elements obtained by applying the callback function to each element of the input array.*/
   return result;
 }
 
