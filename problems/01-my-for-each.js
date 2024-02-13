e051be8a7004bd2db0a4446f41b4cb7cf5850654
Noah <noah.j.ragan@gmail.com>
2024-02-12 19:05:24 -0600

    Completed Problems 1 & 2

--- problems/01-my-for-each.js
+++ problems/01-my-for-each.js
@@ -20,14 +20,23 @@ myForEach(['laika', 'belka'], function (el) {
 });
 console.log(test); // ['LAIKA', 'BELKA']
 *******************************************************************************/
-
+// This line declares a function named myForEach which accepts two parameters: array (the array to be iterated over) and cb (the callback function to be called for each element of the array).
 function myForEach(array, cb) {
-    // Your code here 
+  /*This line starts a for loop that iterates over the elements of the array. It initializes a variable i to 0, and the 
+    loop continues as long as i is less than the length of the array. After each iteration, i is incremented by 1.*/
+  for (let i = 0; i < array.length; i++) {
+    /*Inside the loop, this line calls the callback function cb with three arguments:
+      array[i]: The current element of the array being processed.
+      i: The index of the current element.
+      array: The entire array being iterated.*/
+    cb(array[i], i, array);
+  }
 }
+
 //Noah was here
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
-    module.exports = myForEach;
-} catch(e) {
-    return null;
+  module.exports = myForEach;
+} catch (e) {
+  return null;
 }
--- problems/02-my-map.js
+++ problems/02-my-map.js
@@ -17,12 +17,24 @@ console.log(result2);   // [ 'RUN!', 'FORREST!' ]
 *******************************************************************************/
 
 function myMap(array, cb) {
-    // Your code here 
+  let result = [];
+  for (let i = 0; i < array.length; i++) {
+    result.push(cb(array[i]));
+  }
+  return result;
 }
+
+let result1 = myMap([100, 25, 81, 64], Math.sqrt);
+console.log(result1); // [ 10, 5, 9, 8 ]
+
+let result2 = myMap(["run", "Forrest"], function (el) {
+  return el.toUpperCase() + "!";
+});
+console.log(result2); // [ 'RUN!', 'FORREST!' ]
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
-    module.exports = myMap;
-} catch(e) {
-    return null;
+  module.exports = myMap;
+} catch (e) {
+  return null;
 }
